be6da81e0ab5121b5e747455712f92f6a15d4e87
Richard Nguyen <therichardnguyen@gmail.com>
2019-03-18 16:57:16 -0700

    Initial Commit

---
+++ Homebase Client Engineering Question/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homebase Client Engineering Question
+//
+//  Created by Richard Nguyen on 3/18/19.
+//  Copyright © 2019 Richard Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
